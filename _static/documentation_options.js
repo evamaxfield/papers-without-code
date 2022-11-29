3d96daca87e1dc34ff93a3e75509384fe997f894
evamaxfield <evamaxfieldbrown@gmail.com>
2022-11-29 07:37:12 +0000

    Deploying to gh-pages from @ evamaxfield/papers-without-code@b3ef5f61e8702348dc8e8ef5693fdd107dd7d412 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g4b9c236',
+    VERSION: '0.1.dev1+gb3ef5f6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
